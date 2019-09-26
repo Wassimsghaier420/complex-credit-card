974294f359daae771b641b1267e77938e8e1df68
Wassimsghaier420 <52701776+Wassimsghaier420@users.noreply.github.com>
2019-09-26 10:31:13 +0100

    Updates

---
+++ precache-manifest.7abad4723f60556028ef52e46c4012ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32e7b0b37dda4a7fb8359c84771b17c",
+    "url": "/complex-credit-card/index.html"
+  },
+  {
+    "revision": "e5b3a68ef8f2ec481457",
+    "url": "/complex-credit-card/static/css/main.8e09568f.chunk.css"
+  },
+  {
+    "revision": "1ad0af9cf6a6ad06cee2",
+    "url": "/complex-credit-card/static/js/2.d3fd5e75.chunk.js"
+  },
+  {
+    "revision": "e5b3a68ef8f2ec481457",
+    "url": "/complex-credit-card/static/js/main.148d679b.chunk.js"
+  },
+  {
+    "revision": "dbc0d40d98e5e1dbf4b0",
+    "url": "/complex-credit-card/static/js/runtime~main.0a85308b.js"
+  }
+]);
